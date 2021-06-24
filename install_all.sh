ce904007618eeaa2c7ed4d3c20131cac471ba6bd
Aditya Gupta <ag15035@gmail.com>
2021-06-25 00:34:19 +0530

    Don't reinstall up to date packages

--- install_all.sh
+++ install_all.sh
@@ -5,6 +5,9 @@ set -x
 for package in $(ls)
 do
 	if [ -d "$package" ]; then
-		cd "$package" && makepkg -si && cd ..
+		cd "$package"
+			# Skip failed packages, go on to install/update other packages of the group
+		makepkg -si --needed || true
+		cd ..
 	fi
 done
